f449b0106174594e7bdf31c211e85f479e58528f
Michael Carroll <musicarroll@gmail.com>
2025-08-17 08:05:04 -0600

    Cleanup

---
+++ test_all.sh
@@ -0,0 +1,6 @@
+
+swipl -q -s test_functions.pl -g run_tests,halt -t halt
+swipl -q -s test_json.pl -g run_tests,halt -t halt
+swipl -q -s test_order_set_and_cartesian.pl -g run_tests,halt -t halt
+swipl -q -s test_relations.pl -g run_tests,halt -t halt
+swipl -q -s tests/multiset_tests.pl -g run_tests,halt -t halt
